653d296676bc6883fd4ad7b61926c5b97b90e5bd
thanhlongbm <robocon8a@gmail.com>
2022-07-14 23:16:25 +0700

    update router to index

--- service/src/index.js
+++ service/src/index.js
@@ -1,11 +1,23 @@
 const express = require("express")
 const cors = require("cors")
 require("./database/startDb")
+const productRouter = require("./router/product")
+const customerRouter = require("./router/customer")
+const cartRouter = require("./router/cart")
+const orderRouter = require("./router/order")
+const imageRouter = require("./router/image")
+const supplierRouter = require("./router/supplier")
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
+app.use(productRouter)
+app.use(customerRouter)
+app.use(cartRouter)
+app.use(orderRouter)
+app.use(imageRouter)
+app.use(supplierRouter)
 
 app.listen(PORT, () => console.log("Server is up on port", PORT))
